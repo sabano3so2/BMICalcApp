f465e1963fd2353e37f65418396e302a3fbca04c
sabano3so2 <sabano3so2@gmail.com>
2020-09-27 17:36:04 +0900

    Initial Commit

---
+++ BMICalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalcApp
+//
+//  Created by Masayuki WATANABE on 2020/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
