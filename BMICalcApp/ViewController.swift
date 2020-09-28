2ab85b6de57673590b9a71959184dc2585bd9e58
sabano3so2 <sabano3so2@gmail.com>
2020-09-28 15:36:51 +0900

    BMIのアプリ（ローカルだけなのをコミット）

--- BMICalcApp/ViewController.swift
+++ BMICalcApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UITextField!
+    
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var bmiText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.height.keyboardType = UIKeyboardType.decimalPad
+        self.weight.keyboardType = UIKeyboardType.decimalPad
+        
     }
 
-
+    @IBAction func calcBmi(_ sender: Any) {
+        let dHeight = Double(height.text!)
+        let dWeight = Double(weight.text!)
+        let dHeight2 = dHeight! * dHeight!
+        var bmiRound = (dWeight! / dHeight2) * 100
+        bmiRound = round(bmiRound)
+        
+        let bmi = String(bmiRound / 100)
+        //まるめ（四捨五入）は１００倍にして、Round、その後１００でわる、
+        
+        
+        bmiText.text = ("あなたのBMIは " + bmi + " です！")
+    }
+    
 }
 
